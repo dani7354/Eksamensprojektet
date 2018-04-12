a1d61637ec9a68069ac906ecb3bccc86bc710b96
dani7354 <dani7354@edu.eal.dk>
2018-04-12 13:40:39 +0200

    domain model udkast

--- POWERBANKEN/DataAccess/CSVReader.cs
+++ POWERBANKEN/DataAccess/CSVReader.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+using System.Text;
 namespace DataAccess
 {
-    public class EmptyClass
+    public class CSVReader
     {
-        public EmptyClass()
+        private Encoding _readerEncoding;
+
+
+        public CSVReader()
         {
+            _readerEncoding = Encoding.GetEncoding("iso-8859-1");
+        }
+
+        public string GetProductsFromCSV(string pFilePath)
+        {
+            
+            return string.Empty;
+            
         }
     }
 }
