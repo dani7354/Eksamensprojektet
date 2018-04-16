a2bce435f4060b5785cc1a2bf1e1b7d29a4a880e
Daniel Stuhr Petersen <dani7354@edu.eal.dk>
2018-04-16 08:29:47 +0200

    csv reader

--- POWERBANKEN/DataAccess/CSVReader.cs
+++ POWERBANKEN/DataAccess/CSVReader.cs
@@ -1,22 +1,27 @@
 ﻿using System;
 using System.Text;
+using System.Collections.Generic;
+using System.IO;
 namespace DataAccess
 {
     public class CSVReader
     {
         private Encoding _readerEncoding;
-
-
         public CSVReader()
         {
-            _readerEncoding = Encoding.GetEncoding("iso-8859-1");
+            _readerEncoding = Encoding.GetEncoding("UTF-8");
         }
-
-        public string GetProductsFromCSV(string pFilePath)
+        public List<string> ReadProductsFromCSV(string pFilePath)
         {
-            
-            return string.Empty;
-            
+            List<string> productinfo = new List<string>(); //Udskiftes med en liste af en custom type. evt. på.
+            using (StreamReader reader = new StreamReader(pFilePath, _readerEncoding))
+            {
+                while (reader.EndOfStream == false)
+                {
+                    productinfo.Add(reader.ReadLine());
+                }
+            }
+            return productinfo;
         }
     }
 }
