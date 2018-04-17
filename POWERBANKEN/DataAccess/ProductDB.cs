fab7a4e546e3d728fa6ba20b91197e40654a1333
Rasmus Bak Petersen <rasm105k@edu.eal.dk>
2018-04-17 13:36:23 +0200

    GetBrand

--- POWERBANKEN/DataAccess/ProductDB.cs
+++ POWERBANKEN/DataAccess/ProductDB.cs
@@ -35,8 +35,44 @@ namespace DataAccess
                     int maxStock = (int)reader[6];
                     double productionInHours = (double)reader[7];
                     ProductType productType = new ProductType((string)reader[8]);
-                    
+
                     //Brand brandName = (Brand)reader[9];
+
+                    Product product = new Product(productID, productName, sku, purchasePrice, amount, minStock, maxStock,
+                        productionInHours, productType, null);
+
+                    ProductList.Add(product);
+
+                }
+
+            }
+            return ProductList;
+        }
+        public static List<Product> GetBrand()
+        {
+            List<Product> ProductList = new List<Product>();
+            using (SqlConnection con = DBConnection.Connect)
+            {
+
+                con.Open();
+                SqlCommand cmd1 = new SqlCommand("See_Stock_By_Brand", con);
+                cmd1.CommandType = System.Data.CommandType.StoredProcedure;
+
+                SqlDataReader reader = cmd1.ExecuteReader();
+
+
+                while (reader.Read())
+                {
+                    int productID = (int)reader[0];
+                    string productName = (string)reader[1];
+                    string sku = reader[2].ToString();
+                    double purchasePrice = (double)reader[3];
+                    int amount = (int)reader[4];
+                    int minStock = (int)reader[5];
+                    int maxStock = (int)reader[6];
+                    double productionInHours = (double)reader[7];
+                    ProductType productType = new ProductType((string)reader[8]);
+                    Brand brandName = (Brand)reader[9];
 
                     Product product = new Product(productID, productName, sku, purchasePrice, amount, minStock, maxStock,
                         productionInHours, productType, null);
