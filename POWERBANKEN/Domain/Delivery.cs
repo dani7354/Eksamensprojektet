561ffad059331c9d93e95d8772fd71935a7f5ccb
Simon Egebjerg Jensen <simo897d@edu.eal.dk>
2018-04-17 12:21:07 +0200

    Delivery class

---
+++ POWERBANKEN/Domain/Delivery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class Delivery
+    {
+        public string DeliveryMethod { get; set; }
+        public double DeliveryTime { get; set; }
+    }
+}
