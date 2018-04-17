788a2ec9d433743c6eb4289ea03b69c2c063f4d7
Simon Egebjerg Jensen <simo897d@edu.eal.dk>
2018-04-17 12:18:03 +0200

    Lavet Order class

---
+++ POWERBANKEN/Domain/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class Order
+    {
+        public DateTime OrderDate { get; set; }
+        public DateTime ExpectedDeliveryDate { get; set; }
+        public DateTime ActualDeliveryDate { get; set; }
+        public int Amount { get; set; }
+        public double TotalOrderPrice { get; set; }
+
+
+    }
+}
