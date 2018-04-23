7baa6f1eadbf39a4374b4176816a06623b1257c3
Rasmus Bak Petersen <rasm105k@edu.eal.dk>
2018-04-23 09:29:11 +0200

    h

--- POWERBANKEN/UnitTestProject1/UnitTest2.cs
+++ POWERBANKEN/UnitTestProject1/UnitTest2.cs
@@ -17,17 +17,22 @@ namespace UnitTestProject1
             DifferenceInPercent d = new DifferenceInPercent();
             var result = d.CalculateDifference();
             Assert.AreEqual(0, result);
-            //Algorithms algorithms = new Algorithms();
-            //CSVReader reader = new CSVReader();
-            //DateTime d = new DateTime(2015, 1, 1);
-            //List<int> quantSoldOfAll = new List<int>();
-            //List<int> actualSoldTrue = new List<int>();
-            //var query = reader.ReadProductsSalesInfoFromCSV(@"C:\Users\Simon\source\repos\Eksamensprojektet\Dokumenter\Testdata\Product Sales - 2018-04-19_Jan 2017.csv", d, d).Where(x => x.QuantiySold == 31);
-            //foreach (var item in query)
-            //{
-             
-            //}
-            //Assert.AreEqual(0, algorithms.DifferantialInPercent(31, 0));
+            
+        }
+
+        [TestMethod]
+        public void TestGrowthInPercent()
+        {
+            Algorithms a = new Algorithms();
+
+            int LastYearsSale = 100;
+            double GrowthForPeriod = 1.10;
+            double ExpectedSaleForThisYearsSale = LastYearsSale * GrowthForPeriod;
+            double result = ExpectedSaleForThisYearsSale * GrowthForPeriod;
+           
+           double resultNow = a.GrowthInPercentPerPeriod(LastYearsSale, GrowthForPeriod);
+
+            Assert.AreEqual(result, resultNow);
         }
     }
 }
