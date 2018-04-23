c40f9d7dc67c98b4e24c2d0145d95e7a2ba6433a
Rasmus Bak Petersen <rasm105k@edu.eal.dk>
2018-04-23 11:17:44 +0200

    ForecastCalculation

--- POWERBANKEN/ViewModels/DifferenceInPercent.cs
+++ POWERBANKEN/ViewModels/DifferenceInPercent.cs
@@ -23,26 +23,46 @@ namespace ViewModels
             get { return _actualSale; }
             set { _actualSale = value; }
         }
-        public double DifferantialInPercent(int expectedSale, int actualSale)
+
+        public List<SalesStatistics> ForeCastCalculation(double GrowthInPercent, DateTime month)
         {
-            var a = ProductDB.ReadProductSale();
-            ExpectedSale = expectedSale;
-            ActualSale = actualSale;
-            double result;
-            double r;
-            result = ActualSale - ExpectedSale;
-            r = result / ExpectedSale;
-            result = r * 100;
-            return result;
+            GrowthInPercent = (GrowthInPercent / 100) + 1;
+            List<SalesStatistics> ForecastList = new List<SalesStatistics>();
+            List<SalesStatistics> monthOftheYear = ProductDB.ReadProductSale().Where(x => x.Start.Month == month.Month).ToList();
+            foreach (var stat in monthOftheYear)
+            {
+                int result = (int)Math.Ceiling(stat.QuantitySold * GrowthInPercent);
+                ForecastList.Add(new SalesStatistics()
+                {
+                    QuantitySold = result,
+                    Start = stat.Start.AddYears(1),
+                    End = stat.End.AddYears(1),
+                    Product = stat.Product
+                });
+            }
+            return ForecastList;
         }
 
-        public double GrowthInPercentPerPeriod(int LastYearssale, double growthForPeriod)
-        {
-            double ExpectedSaleForThisYearsPeriod = LastYearssale * growthForPeriod;
+        //public double DifferantialInPercent(int expectedSale, int actualSale)
+        //{
+        //    var a = ProductDB.ReadProductSale();
+        //    ExpectedSale = expectedSale;
+        //    ActualSale = actualSale;
+        //    double result;
+        //    double r;
+        //    result = ActualSale - ExpectedSale;
+        //    r = result / ExpectedSale;
+        //    result = r * 100;
+        //    return result;
+        //}
 
-            double result = ExpectedSaleForThisYearsPeriod * growthForPeriod;
+        //public double GrowthInPercentPerPeriod(int LastYearssale, double growthForPeriod)
+        //{
+        //    double ExpectedSaleForThisYearsPeriod = LastYearssale * growthForPeriod;
 
-            return result;
-        }
+        //    double result = ExpectedSaleForThisYearsPeriod * growthForPeriod;
+
+        //    return result;
+        //}
     }
 }
