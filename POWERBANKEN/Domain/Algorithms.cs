3787244d24c19157b234f2b2d5fdd48899a4c8ed
Rasmus Bak Petersen <rasm105k@edu.eal.dk>
2018-04-24 09:22:44 +0200

    Algoritme genindsat

---
+++ POWERBANKEN/Domain/Algorithms.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Algorithms
+    {
+        private int _expectedSale;
+        private int _actualSale;
+        public int ExpectedSale
+        {
+            get { return _expectedSale; }
+            set { _expectedSale = value; }
+        }
+        public int ActualSale
+        {
+            get { return _actualSale; }
+            set { _actualSale = value; }
+        }
+        public double DifferantialInPercent(int expectedSale, int actualSale)
+        {
+            ExpectedSale = expectedSale;
+            ActualSale = actualSale;
+            double result;
+            double r;
+            result = ActualSale - ExpectedSale;
+            r = result / ExpectedSale;
+            result = r * 100;
+            return result;
+        }
+    }
+}
