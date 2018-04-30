10555f0ea172ad06e2a17fef5aeffa0fdbbc1f59
Simon Egebjerg Jensen <simo897d@edu.eal.dk>
2018-04-30 10:37:47 +0200

    Test lavet til AddProduct

---
+++ POWERBANKEN/UnitTestProject1/AddProductTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ViewModels;
+using Domain;
+using DataAccess;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class AddProductTest
+    {
+        [TestMethod]
+        public void TestAddingProduct()
+        {
+            string sku = "T105";
+            string name = "Power2000";
+            double purchasePrice = 20;
+            int amount = 100;
+            int minAmount = 10;
+            int productTypeID = 1;
+            int brandID = 1;
+            double leadTime = 50;
+            bool isActive = true;
+            ViewModels.AddProductViewModel apvm = new AddProductViewModel();
+
+            apvm.AddAProduct(sku, name, purchasePrice, amount, minAmount, productTypeID, brandID, leadTime, isActive);
+
+            ProductDB db = new ProductDB();
+            Assert.IsTrue(db.GetAllProducts().Exists(p => p.SKU == sku));
+        }
+    }
+}
---
+++ POWERBANKEN/ViewModels/AddProductViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using
+namespace ViewModels
+{
+    public class AddProductViewModel
+    {
+        
+        public void AddAProduct(string sku, string name, double purchasePrice, int amount, int minAmount, int productTypeID, int brandID, double leadTime, bool isActive)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
