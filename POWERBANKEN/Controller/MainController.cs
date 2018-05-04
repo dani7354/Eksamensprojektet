50535aeeaf006e4f3e2e908f9788d994ab720ca1
Daniel Stuhr Petersen <dani7354@edu.eal.dk>
2018-05-04 10:12:44 +0200

    OrderDateCalculator: Tjekker for dagligt salgstal kun hvis måneden er skiftet

--- POWERBANKEN/Controller/MainController.cs
+++ POWERBANKEN/Controller/MainController.cs
@@ -10,7 +10,6 @@ namespace Controller
 {
     public class MainController
     {
-        private static readonly MainController _instance = new MainController();
         private IDataStorage _dataStorage;
         private TxtAccess _txtAccess;
         private List<Product> _products;
@@ -22,15 +21,9 @@ namespace Controller
 			_dataStorage = new ProductDB(); _productSales = _dataStorage.GetProductSales();
 			_products = _dataStorage.GetAllProducts(); _txtAccess = new TxtAccess();
 		}
-        public static MainController Instance
-        {
-            get
-            {
-                return _instance;
-            }
-        }
+        public static MainController Instance { get; } = new MainController();
 
-		public void AddProduct(Product product)
+        public void AddProduct(Product product)
         {
             _dataStorage.InsertProduct(product);
             _products.Add(product);
