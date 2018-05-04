ba0b91948d8960b11df32602bd544d26c9b50880
Jakob Vingaard <jakob2665@gmail.com>
2018-05-04 15:50:03 +0200

    update

--- POWERBANKEN/ViewModels/SalesChartViewModel.cs
+++ POWERBANKEN/ViewModels/SalesChartViewModel.cs
@@ -4,19 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Domain;
-using DataAccess;
+using Controller;
+using Syncfusion.UI.Xaml.Charts;
 
 namespace ViewModels
 {
-   public class SalesChartViewModel
+   public class SalesChartViewModel : BaseViewModel
     {
-        public List<SalesStatistics> StatistikPDB { get; set; }
+        public List<SalesStatistics> StatistikPDB
+        {
+            get { return _statistikPDB; }
+            set
+            {
+                _statistikPDB = value; NotifyPropertyChanged("StatistikPDB");
+            }
+        }
 
-        private ProductDB pdb = new ProductDB();
+        public List<Product>PDName { get; set; }
+
+        public List<Brand>Brands { get; set; }
+        
+        public Brand SelectedBrand {
+            get { return _selectedBrand; }
+            set { _selectedBrand = value;SetStat(); NotifyPropertyChanged("SelectedBrand"); }
+
+        }
+        public void SetStat() { StatistikPDB = mc.GetProductSales().Where(S => S.Product.Brand.Name == SelectedBrand.Name).ToList(); 
+    }
+        public MainController mc = MainController.Instance;
+        private Brand _selectedBrand;
+        private List<SalesStatistics> _statistikPDB;
 
         public SalesChartViewModel()
         {
-            StatistikPDB = pdb.GetProductSales();
+            StatistikPDB = mc.GetProductSales();
+            PDName = mc.GetProducts();
+            Brands = mc.GetBrands();
         }
             
     }
