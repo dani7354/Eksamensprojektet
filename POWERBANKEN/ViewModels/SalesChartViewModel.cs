531ce05647c02874d66c10e1e49a9b5f7d4b3e34
Jakob Vingaard <jakob2665@gmail.com>
2018-05-07 13:49:25 +0200

    Update Mandag

--- POWERBANKEN/ViewModels/SalesChartViewModel.cs
+++ POWERBANKEN/ViewModels/SalesChartViewModel.cs
@@ -5,18 +5,35 @@ using System.Text;
 using System.Threading.Tasks;
 using Domain;
 using Controller;
-using Syncfusion.UI.Xaml.Charts;
+
 
 namespace ViewModels
 {
    public class SalesChartViewModel : BaseViewModel
     {
+        public SalesChartViewModel()
+        {
+            StatistikPDB = mc.GetProductSales();
+            PDName = mc.GetProducts();
+            Brands = mc.GetBrands();
+            Setproduct();
+        }
+            
         public List<SalesStatistics> StatistikPDB
         {
             get { return _statistikPDB; }
             set
             {
                 _statistikPDB = value; NotifyPropertyChanged("StatistikPDB");
+            }
+        }
+
+        private void Setproduct()
+        {
+            foreach (var item in StatistikPDB.Where(S=>PDName.Exists(p=>p.SKU == S.Product.SKU)))
+            {
+                var SKU = item.Product.SKU;
+                item.Product = PDName.Where(p => p.SKU == SKU).First();
             }
         }
 
@@ -29,18 +46,11 @@ namespace ViewModels
             set { _selectedBrand = value;SetStat(); NotifyPropertyChanged("SelectedBrand"); }
 
         }
-        public void SetStat() { StatistikPDB = mc.GetProductSales().Where(S => S.Product.Brand.Name == SelectedBrand.Name).ToList(); 
+        public void SetStat() { StatistikPDB = mc.GetProductSales().Where(S => S.Product.Brand != null && S.Product.Brand.Name == SelectedBrand.Name).ToList(); 
     }
         public MainController mc = MainController.Instance;
         private Brand _selectedBrand;
         private List<SalesStatistics> _statistikPDB;
 
-        public SalesChartViewModel()
-        {
-            StatistikPDB = mc.GetProductSales();
-            PDName = mc.GetProducts();
-            Brands = mc.GetBrands();
-        }
-            
     }
 }
